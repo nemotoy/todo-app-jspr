ea4141338962d62476e358a79dc17b63cb41d3ad
yuma-nemoto <yuma-nemoto@dac.co.jp>
2020-08-25 16:21:58 +0900

    test: implement isEmptyTitle()

---
+++ test/TodoItemModel.js
@@ -0,0 +1,14 @@
+import assert  from "assert";
+import { TodoItemModel } from "../src/model/TodoItemModel.js";
+
+it("Given title isn't empty, isEmptyTitle return false", () => {
+    const input = new TodoItemModel({ title: "title", completed: false })
+    const expected = false
+    assert.strictEqual(input.isEmptyTitle(), expected)
+});
+
+it("Given title is empty, isEmptyTitle return true", () => {
+    const input = new TodoItemModel({ title: "", completed: false })
+    const expected = true
+    assert.strictEqual(input.isEmptyTitle(), expected)
+});
